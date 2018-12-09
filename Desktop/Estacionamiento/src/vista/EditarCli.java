72564e67403606ff251e17b84dd61e4474260795
Leonardo Muñoz <leonardo.munoz33@inacapmail.cl>
2018-12-09 20:43:13 -0300

    Se modifica codigo para respetar MVC

--- Desktop/Estacionamiento/src/vista/EditarCli.java
+++ Desktop/Estacionamiento/src/vista/EditarCli.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vista;
 
 import controlador.Registro;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import modelo.Clientes;
 import sql.Conexion;
