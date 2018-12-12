0fe5182363df2afcfe30eb336ec13a0048c6ffe6
Leonardo Muñoz <leonardo.munoz33@inacapmail.cl>
2018-12-12 01:36:53 -0300

    Nuevo proyecto

--- Desktop/ProyectoEstacionamiento/src/BD/Conexion.java
+++ Desktop/ProyectoEstacionamiento/src/BD/Conexion.java
@@ -1,6 +1,11 @@
 
 package BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author grupo
@@ -8,7 +13,39 @@ package BD;
 public class Conexion {
     
     
+    public static String bd     =   "estacionamiento";
+    public static String login  =   "root";
+    public static String pass   =   "";
     
+    public static String url    =   "jdbc:mysql://localhost/" + bd;
+    
+    public static Connection conn; //realiza la conexion
+    public static Statement sentencia; //camino de las consultas
+    
+    public static boolean buscarPatente;
+   
+    public static Connection getConexion(){
+    
+           
+        Connection conn = null;
+        try {
+            String driverClassName = "com.mysql.jdbc.Driver";
+            Class.forName(driverClassName);
+            conn = DriverManager.getConnection(url, login, pass);
+            System.out.println("Base de datos conectada!!");
+            
+        } catch (Exception e) {
+            
+            System.out.println(e);
+            
+        }
+      return conn;
+    }
+    
+    
+    public static void Desconectar() throws SQLException{
+        conn.close();
+    }
     
     
 }
